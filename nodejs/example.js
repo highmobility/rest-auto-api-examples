dc312aef77aa7d9c58008e88262350ac05ac7686
Kevin Valdek <kevinvaldek@gmail.com>
2022-12-08 09:57:37 +0100

    updated example to v5

--- nodejs/example.js
+++ nodejs/example.js
@@ -1,26 +1,25 @@
 // Replace the config with your snippet
 var REST_API_CONFIG =  {
-  "version": "1.2",
+  "version": "3.0",
   "type": "rest_api",
-  "private_key_id": "f46e9a6e-4dfc-4fc2-8d43-78bf33bfdf6d",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgdyHevWjV81hMC3r8\njc50YoP6xIryDMeFTOEAuwOyzkihRANCAAQ8cx5XXnDgiLcrRxLHQQOX6L+1abEb\nSc+x/Si2hJp5Kp6ZHDOAcTF2ikSGsysxHrc0XG6nLPYS0UF15jieojHj\n-----END PRIVATE KEY-----\n\n",
-  "app_uri": "https://sandbox.rest-api.high-mobility.com/v3",
-  "app_id": "08F06CDB482DE3575278C31F"
+  "private_key_id": "e6881b32-dec6-4d56-8bff-c8462eb070d6",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgf98dg89RnunSQlEi\n9IqlSkvmtnb9uqU+wIez0cQTkYyhRANCAARv1elcg9Q/4H+e6AgXob59MN4n30Lr\nE7au3oKP4ginfkWjhrJX5ihXduqGyiUIezMKe5oQCMw4BLyqlQn+1oDX\n-----END PRIVATE KEY-----\n\n",
+  "app_uri": "https://rest-api.high-mobility.com/v5",
+  "app_id": "F2F7C9D6A1C409AEF4FCE226",
+  "client_serial_number": "2BA0AFB864121DF62D"
 }
-
-
 
 var jwt = require('jsonwebtoken')
 var uuid4 = require('uuid4');
 
 var payload = {
-  'api_version': REST_API_CONFIG.version,
-  'app_id': REST_API_CONFIG.app_id,
-  'aud': REST_API_CONFIG.app_uri,
-  'iat': Math.round(Date.now()/1000),
-  'jti': uuid4(),
-  'access_token': '0c4c3cd1-fe5c-4b7a-85dd-cb86e548f29e'
-};
+  ver: REST_API_CONFIG.version,
+  iss: REST_API_CONFIG.client_serial_number,
+  aud: REST_API_CONFIG.app_uri,
+  iat: Math.round(Date.now() / 1000),
+  jti: uuid4(),
+  sub: 'b55af95a-607c-47e8-ad37-5016a8beda61'
+}
 
 var privateKey = Buffer.from(REST_API_CONFIG.private_key, 'utf8')
 var jwtToken = jwt.sign(payload, privateKey, { algorithm: 'ES256' })
